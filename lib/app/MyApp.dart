03b05dcaacd7216a4b77e33c14731b4f8eae6575
Wladimir Bandeira <tecnico100@gmail.com>
2022-10-09 13:05:00 -0300

    first commit

---
+++ lib/app/MyApp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  MyApp._internal();
+
+  static final MyApp instance = MyApp._internal();
+
+  factory MyApp() => instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_ecommerce/app/MyApp.dart';
+
+void main() {
+  runApp(MyApp());
+}
