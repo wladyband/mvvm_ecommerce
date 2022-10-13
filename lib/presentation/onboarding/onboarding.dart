e6ab2387993250c7f54f6fe99013d328c34f225e
Wladimir Bandeira <tecnico100@gmail.com>
2022-10-13 13:39:25 -0300

    criação das rotas e paginas do projeto

---
+++ lib/presentation/onboarding/onboarding.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnboardingView extends StatefulWidget {
+  const OnboardingView({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingView> createState() => _OnboardingViewState();
+}
+
+class _OnboardingViewState extends State<OnboardingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
