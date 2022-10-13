383adfe731e4b2030da80aa406d4aac91d4c6c03
Wladimir Bandeira <tecnico100@gmail.com>
2022-10-13 15:39:29 -0300

    criando efeito de transição entre telas em relação a tela inicial

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_ecommerce/presentation/resources/color_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -10,6 +11,9 @@ class OnBoardingView extends StatefulWidget {
 class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      body: const Center(child:  Text("Welcome to on boarding")),
+    );
   }
 }
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:mvvm_ecommerce/presentation/resources/routes_manager.dart';
+
 import '/presentation/resources/assets_manager.dart';
 import '/presentation/resources/color_manager.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +14,28 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(const Duration(seconds: 2), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
