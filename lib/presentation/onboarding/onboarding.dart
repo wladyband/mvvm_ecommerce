3bc1b930466f6c6a7ec8929219c1126ca7950dd5
Wladimir Bandeira <tecnico100@gmail.com>
2022-10-14 07:15:30 -0300

    implementando a primeira parte de OnBoardingView

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mvvm_ecommerce/domain/model/model.dart';
+import 'package:mvvm_ecommerce/presentation/resources/assets_manager.dart';
 import 'package:mvvm_ecommerce/presentation/resources/color_manager.dart';
+import 'package:mvvm_ecommerce/presentation/resources/strings_manager.dart';
+import 'package:mvvm_ecommerce/presentation/resources/values_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -9,11 +14,44 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  PageController _pageController = PageController(initialPage: 0);
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+    SliderObject(AppStrings.onBoardingSubTitle1,
+        AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
+    SliderObject(AppStrings.onBoardingSubTitle2,
+        AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
+    SliderObject(AppStrings.onBoardingSubTitle3,
+        AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
+    SliderObject(AppStrings.onBoardingSubTitle4,
+        AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: const Center(child:  Text("Welcome to on boarding")),
+      appBar: AppBar(
+        elevation: AppSize.s1_5,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(
+          controller: _pageController,
+          itemCount: _list.length,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          itemBuilder: (context,index){
+            // return OnBoardingPage
+          }),
     );
   }
 }
