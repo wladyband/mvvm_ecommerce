2f34d277470e66a289f9ddcea2af3582ea5071c6
Wladimir Bandeira <tecnico100@gmail.com>
2022-11-13 11:51:10 -0300

    iniciando implementação do controller para o login

--- lib/domain/usecase/login_usecase.dart
+++ lib/domain/usecase/login_usecase.dart
@@ -1,9 +1,10 @@
 import 'package:dartz/dartz.dart';
+import 'package:mvvm_ecommerce/app/functions.dart';
 import 'package:mvvm_ecommerce/data/request/network/failure.dart';
 import 'package:mvvm_ecommerce/data/request/request.dart';
-import 'package:mvvm_ecommerce/data/usecase/base_usecase.dart';
 import 'package:mvvm_ecommerce/domain/model/model.dart';
 import 'package:mvvm_ecommerce/domain/respository/repository.dart';
+import 'package:mvvm_ecommerce/domain/usecase/base_usecase.dart';
 
 class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
   Repository _repository;
@@ -11,10 +12,10 @@ class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
   LoginUseCase(this._repository);
 
   @override
-  Future<Either<Failure, Authentication>> execute(
-      LoginUseCaseInput input) async {
-    await _repository
-        .login(LoginRequest(input.email, input.password, "imei", "deviceType"));
+  Future<Either<Failure, Authentication>> execute(LoginUseCaseInput input) async {
+    DeviceInfo deviceInfo = await getDeviceDetails();
+    return await _repository
+        .login(LoginRequest(input.email, input.password, deviceInfo.identifier, deviceInfo.name));
   }
 }
 
